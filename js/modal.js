b39b978f3725e947c87509e279114ceb20082937
Aleks <alekssson@gmail.com>
2019-11-04 11:52:10 +0300

    иконки соц.сетей

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,8 @@
-"use strict"
+"use strict";
 
 var contacts_link = document.querySelector(".contacts .button");
 var popup = document.querySelector(".feedback");
-var close_btn = popup.querySelector(".feedback-close");
+var close_btn = document.querySelector(".feedback-close");
 
 contacts_link.addEventListener("click", function(event){
   event.preventDefault();
@@ -12,4 +12,13 @@ contacts_link.addEventListener("click", function(event){
 close_btn.addEventListener("click", function(event){
   event.preventDefault();
   popup.classList.remove("feedback-show");
-})
+});
+
+window.addEventListener("keydown", function (event) {
+    if (event.keyCode === 27) {
+      event.preventDefault();
+      if (popup.classList.contains("feedback-show")) {
+        popup.classList.remove("feedback-show");
+      }
+    }
+  });
